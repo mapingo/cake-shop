a706585892c0a33e93fc8996b4134b3ca709cf73
allanmckenzie <allan.mckenzie@hmcts.net>
2025-05-02 16:06:15 +0100

    Add dummy class to stop liquibase complaining about missing classes in classes directory

---
+++ cakeshop-integration-test/src/main/java/uk/gov/justice/services/cakeshop/it/DummyClassToStopLiquibaseComplainingAboutMissingClasses.java
@@ -0,0 +1,10 @@
+package uk.gov.justice.services.cakeshop.it;
+
+public class DummyClassToStopLiquibaseComplainingAboutMissingClasses {
+
+    private boolean DUMMY_CLASS_TO_STOP_LIQUIBASE_COMPLAINING_ABOUT_MISSING_CLASSES_IN_CLASSES_DIRECTORY = true;
+
+    public boolean shouldStopComplaining() {
+        return DUMMY_CLASS_TO_STOP_LIQUIBASE_COMPLAINING_ABOUT_MISSING_CLASSES_IN_CLASSES_DIRECTORY;
+    }
+}
